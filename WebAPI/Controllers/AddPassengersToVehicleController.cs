8483a65070a779c1543ce9fd201ea9e696bdf245
Cameron Lambert <cameronlambert84@gmail.com>
2023-01-30 11:48:57 +0000

    namespaces file scoped

--- WebAPI/Controllers/AddPassengersToVehicleController.cs
+++ WebAPI/Controllers/AddPassengersToVehicleController.cs
@@ -1,34 +1,33 @@
 ﻿using Domain.Models.Vehicles;
 
-namespace WebApi.Controllers
+namespace WebApi.Controllers;
+
+[Route("api/[controller]/[action]")]
+[ApiController]
+public class AddPassengersToVehicleController : ControllerBase
 {
-    [Route("api/[controller]/[action]")]
-    [ApiController]
-    public class AddPassengersToVehicleController : ControllerBase
-    {
-        [HttpGet]
-        public IActionResult AddPassengersToCar(int number)
+    [HttpGet]
+    public IActionResult AddPassengersToCar(int number)
         => StartVehicleWithEmptyPeopleRequest<Car>(number);
 
-        [HttpGet]
-        public IActionResult AddPassengersToMotorBike(int number)
-            => StartVehicleWithEmptyPeopleRequest<MotorBike>(number);
+    [HttpGet]
+    public IActionResult AddPassengersToMotorBike(int number)
+        => StartVehicleWithEmptyPeopleRequest<MotorBike>(number);
 
-        [HttpGet]
-        public IActionResult AddPassengersToCoach(int number)
-            => StartVehicleWithEmptyPeopleRequest<Coach>(number);
+    [HttpGet]
+    public IActionResult AddPassengersToCoach(int number)
+        => StartVehicleWithEmptyPeopleRequest<Coach>(number);
 
-        private IActionResult StartVehicleWithEmptyPeopleRequest<T>(int number) where T : Vehicle, new()
+    private IActionResult StartVehicleWithEmptyPeopleRequest<T>(int number) where T : Vehicle, new()
+    {
+        try
         {
-            try
-            {
-                new T().StartWithEmptyPeople(number);
-                return Ok();
-            }
-            catch (Exception ex)
-            {
-                return BadRequest(ex.Message);
-            }
+            new T().StartWithEmptyPeople(number);
+            return Ok();
+        }
+        catch (Exception ex)
+        {
+            return BadRequest(ex.Message);
         }
     }
 }
